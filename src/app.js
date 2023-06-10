12cbc8812b2edd87e22b8f1c6c2c6bca1227fab9
lugras <69491958+lugras@users.noreply.github.com>
2023-06-10 22:45:55 +0000

    123

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/*Creating the array to pick card symbols*/
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onclick = function myClickHandler() {
+  /*Function to pick a random position of arr*/
+  function randomCard(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  //Identifying the ramdonm position of arr from the last function
+  let whose = who[randomCard(who)];
+  let actions = action[randomCard(action)];
+  let whats = what[randomCard(what)];
+  let whens = when[randomCard(when)];
+  let wholexcuse = whose + " " + actions + " " + whats + " " + whens;
+  console.log("wholexcuse");
+  //Searching the simbols to be replaced and print them on the page
+  let card = document.querySelector("#excuse");
+
+  card.innerHTML = wholexcuse;
+
+  //console.log("$whose", "$actions");
 };
